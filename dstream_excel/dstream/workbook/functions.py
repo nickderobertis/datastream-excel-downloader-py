21db2a10e9b6be8294ad67c2725f4b00a99fd365
Nick DeRobertis <whoopnip@gmail.com>
2019-10-01 18:30:05 -0400

    Allow multiple symbols in datastream function

--- dstream_excel/dstream/workbook/functions.py
+++ dstream_excel/dstream/workbook/functions.py
@@ -1,18 +1,20 @@
-from typing import Sequence
+from typing import Sequence, Union
 
 class DatastreamExcelFunction:
-    def __init__(self, symbols):
+    def __init__(self, symbols: Union[Sequence[str], str]):
         self.symbols = symbols
 
-    def time_series(self, variables, begin='-2Y', end='', freq='D'):
+    def time_series(self, variables: Sequence[str], begin='-2Y', end='', freq='D'):
         return _datastream_excel_function(self.symbols, variables, begin=begin, end=end, freq=freq)
 
-    def static(self, variables):
+    def static(self, variables: Sequence[str]):
         return _datastream_excel_function(self.symbols, variables, begin='Latest Value', end='', freq='')
 
 
-def _datastream_excel_function(symbols, variables: Sequence[str], begin, end, freq):
+def _datastream_excel_function(symbols: Union[Sequence[str], str], variables: Sequence[str], begin, end, freq):
     variables_str = ';'.join(variables)
+    if isinstance(symbols, (list, tuple)):
+        symbols = ','.join(symbols)
     middle_str = '","'.join([symbols, variables_str, begin, end, freq])
     custom_header_str = 'CustomHeader=true;CustHeaderDatatypes=DATATYPE'
     headers_str = f'RowHeader=true;ColHeader=false'
